881b7b1de277f85501f00eb5dc8a3cb425bf1aa0
Yash Kumar Gupta <ykgupta2411@gmail.com>
2024-04-17 22:04:30 +0530

    Backend updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://ykgupta2411:NkJwmIPPS3uU4qtN@database.8mncj2h.mongodb.net/";
 
 const connectToDatabase = () => {
-    mongoose.connect(uri).then(() => {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log("MongoDB Connected Successfully!");
     }).catch((error) => {
         console.log("Error when connecting to MongoDB: " + error);
