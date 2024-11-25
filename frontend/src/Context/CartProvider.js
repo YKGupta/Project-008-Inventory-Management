71bc99ad3d19ff5e51457e881b4e974caf5e3f72
Yash Kumar Gupta <ykgupta2411@gmail.com>
2024-11-25 19:22:52 +0530

    Added Payment Gateway

--- frontend/src/Context/CartProvider.js
+++ frontend/src/Context/CartProvider.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import CartContext from "./CartContext";
 import AlertContext from "./AlertContext";
 import ProgressContext from "./ProgressContext";
+import loadScript from "../Utility/loadScript";
 
 const CartProvider = (props) => {
 
@@ -16,57 +17,14 @@ const CartProvider = (props) => {
 
     const getAllOrders = async () => {
 
-		setProgress(10);		
-		
-		try
-		{
-			const response = await fetch(`${host}/api/order/getall${localStorage.getItem('admin') === 'false' ? 'user' : ''}`, {
-				method: "GET",
-				headers: {
-					"Auth-Token": localStorage.getItem('token')
-				}
-			});
-
-			setProgress(50);
-			
-			const json = await response.json();
-			
-			setProgress(90);
-
-			if(json.success)
-			{
-				setOrders(json.orders);
-			}
-			else
-			{
-				alertSetter({ message: json.message, type: "danger"});
-			}
-		}
-		catch(error)
-		{
-			alertSetter({ message: error, type: "danger"});
-		}
-		finally
-		{
-			setProgress(100);		
-		}
-	}
-
-    const placeOrder = async () => {
-
         setProgress(10);
 
         try {
-            const response = await fetch(`${host}/api/order/add`, {
-                method: "POST",
+            const response = await fetch(`${host}/api/order/getall${localStorage.getItem('admin') === 'false' ? 'user' : ''}`, {
+                method: "GET",
                 headers: {
-                    "Content-Type": "application/json",
                     "Auth-Token": localStorage.getItem('token')
-                },
-                body: JSON.stringify({
-                    items: cartItems,
-                    totalPrice: totalCost
-                })
+                }
             });
 
             setProgress(50);
@@ -76,15 +34,116 @@ const CartProvider = (props) => {
             setProgress(90);
 
             if (json.success) {
-                // Update frontend
-                alertSetter({ message: json.message, type: "warning" });
-                setCartItems([]);
-                navigate('/orders');
+                setOrders(json.orders);
             }
             else {
                 alertSetter({ message: json.message, type: "danger" });
             }
+        }
+        catch (error) {
+            alertSetter({ message: error, type: "danger" });
+        }
+        finally {
+            setProgress(100);
+        }
+    }
 
+    const placeOrder = async () => {
+
+        setProgress(10);
+
+        try 
+        {
+            const res = await loadScript("https://checkout.razorpay.com/v1/checkout.js");
+
+            if (!res) {
+                alertSetter({ message: "Unable to connect", type: "danger" });
+                return;
+            }
+
+            const orderResponse = await fetch(`${host}/api/payment/create`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Auth-Token": localStorage.getItem('token')
+                },
+                body: JSON.stringify({
+                    totalAmount: totalCost
+                })
+            });
+
+            const orderData = await orderResponse.json();
+
+            const { amount, id: order_id, currency } = orderData.order;
+
+            const options = {
+                key: "rzp_test_ZYDPQ7TGPxay6A", // Enter the Key ID generated from the Dashboard
+                amount: amount.toString(),
+                currency: currency,
+                name: "Test",
+                description: "Test Transaction",
+                order_id: order_id,
+                handler: async function (response) {
+                    const data = {
+                        orderCreationId: order_id,
+                        razorpayPaymentId: response.razorpay_payment_id,
+                        razorpayOrderId: response.razorpay_order_id,
+                        razorpaySignature: response.razorpay_signature,
+                    };
+                    const r = await fetch(`${host}/api/payment/verify`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Auth-Token": localStorage.getItem('token')
+                        },
+                        body: JSON.stringify(data)
+                    });
+                    const d = await r.json();
+                    if(d.success)
+                    {
+                        const response = await fetch(`${host}/api/order/add`, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                "Auth-Token": localStorage.getItem('token')
+                            },
+                            body: JSON.stringify({
+                                items: cartItems,
+                                totalPrice: totalCost
+                            })
+                        });
+            
+                        setProgress(50);
+            
+                        const json = await response.json();
+            
+                        setProgress(90);
+            
+                        if (json.success) {
+                            // Update frontend
+                            alertSetter({ message: json.message, type: "warning" });
+                            setCartItems([]);
+                            navigate('/orders');
+                        }
+                        else {
+                            alertSetter({ message: json.message, type: "danger" });
+                        }
+                    }
+                },
+                prefill: {
+                    name: "Test",
+                    email: "test@example.com",
+                    contact: "9999999999",
+                },
+                notes: {
+                    address: "Test",
+                },
+                theme: {
+                    color: "#61dafb",
+                },
+            };
+            const paymentObject = new window.Razorpay(options);
+            paymentObject.open();
         }
         catch (error) {
             alertSetter({ message: error, type: "danger" });
